81ddeb94d1395da13d63e5d1ea3bc3dd5ed7fad3
Spencer-Lake <spencer.m.lake11@gmail.com>
2021-11-04 17:29:17 -0400

    Your approach tool

---
+++ src/Header.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import "./Header.css"
+
+export default function header () {
+
+    const menuBtn = document.querySelector('.menu-btn')
+    let menuOpen = false
+    
+    menuBtn.addEventListener('click', () => {
+        if(!menuOpen) {
+            menuBtn.classList.add('open')
+            menuOpen = true
+        } else {
+            menuBtn.classList.remove('open')
+            menuOpen = false
+        }
+    })
+
+    return(
+        <section>
+            <header>
+                <div className='menu-btn'>
+                    <div className='menu-btn__burger'></div>
+                </div>
+                <div className='profile-pic'>
+
+                </div>
+            </header>
+        </section>
+    )
+}
