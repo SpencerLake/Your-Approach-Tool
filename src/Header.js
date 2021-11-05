bcb8bc7ad1642e875ffafece97b5a084194bac2e
Spencer-Lake <spencer.m.lake11@gmail.com>
2021-11-05 11:31:49 -0400

    updated

--- src/Header.js
+++ src/Header.js
@@ -5,16 +5,16 @@ export default function header () {
 
     const menuBtn = document.querySelector('.menu-btn')
     let menuOpen = false
-    
-    menuBtn.addEventListener('click', () => {
-        if(!menuOpen) {
-            menuBtn.classList.add('open')
-            menuOpen = true
-        } else {
-            menuBtn.classList.remove('open')
-            menuOpen = false
-        }
-    })
+
+    // menuBtn.addEventListener('click', () => {
+    //     if(!menuOpen) {
+    //         menuBtn.classList.add('open')
+    //         menuOpen = true
+    //     } else {
+    //         menuBtn.classList.remove('open')
+    //         menuOpen = false
+    //     }
+    // })
 
     return(
         <section>
@@ -22,9 +22,8 @@ export default function header () {
                 <div className='menu-btn'>
                     <div className='menu-btn__burger'></div>
                 </div>
-                <div className='profile-pic'>
-
-                </div>
+                <div className='logo'></div>
+                <div className='profile-pic'></div>
             </header>
         </section>
     )
